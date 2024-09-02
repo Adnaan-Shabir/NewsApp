ed4c63dea365a8000edeaa74186bbe876555bfc9
Adnan Sheikh <84373219+Adnaan-Shabir@users.noreply.github.com>
2024-09-02 18:42:46 +0530

    Update settings.py

--- newsapp/settings.py
+++ newsapp/settings.py
@@ -117,10 +117,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR,  'static')
-] 
-
 
 # NEWS_API_KEY
 NEWS_API_KEY = 'YOUR API KEY'
